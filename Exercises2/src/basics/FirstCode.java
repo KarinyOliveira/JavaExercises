c0f0838aba2b88c2bac0b60bf61399a841536edd
Kariny Oliveira <karinyoliveira@KarinyPC.hitronhub.home>
2024-05-05 11:49:26 -0700

    TA-01: Project Hello World

---
+++ Exercises2/src/basics/FirstCode.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class FirstCode {
+    public static void main(String[] args) {
+
+        //shortcut to print in IntelliJ sout
+        System.out.println("Hello World");
+
+    }
+}
