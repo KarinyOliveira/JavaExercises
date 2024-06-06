8e9deee8ad41a220cd52889f859a0389c79f2c61
Kariny Oliveira <kmoliveira0@gmail.com>
2024-06-06 10:59:52 -0700

    Java Basic Casting

--- Exercises2/src/basics/PrimitiveTypeConvertion.java
+++ Exercises2/src/basics/PrimitiveTypeConvertion.java
@@ -3,7 +3,20 @@ package basics;
 public class PrimitiveTypeConvertion {
     public static void main(String[] args) {
 
-        double a = 1;
+        double a = 1; // implicit convertion
         System.out.println(a);
+
+        float b = (float) 1.0; // explicit conversion
+        System.out.println(b);
+
+        int c = 340;
+        byte d = (byte) c; //Casting
+        System.out.println(d);
+
+        double e = 1.999999;
+        int f = (int) e;
+        System.out.println(f);
+
+
     }
 }
